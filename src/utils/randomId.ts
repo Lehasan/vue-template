70467733f10361bd748a8140a5d3761299a299fe
Lehasan <oa5213233@gmail.com>
2024-12-05 17:27:48 +0200

    vue-template

--- src/utils/randomId.ts
+++ src/utils/randomId.ts
@@ -1,1 +1,14 @@
-export const randomId = () => Math.random().toString(36).slice(2, 6)
+export const randomId = (size = 5) => {
+	let id = ''
+
+	const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-'
+	const alphabetLength = alphabet.length
+
+	const randomValues = crypto.getRandomValues(new Uint8Array(size))
+
+	for (let i = 0; i < size; i++) {
+		id += alphabet[randomValues[i] % alphabetLength]
+	}
+
+	return id
+}
